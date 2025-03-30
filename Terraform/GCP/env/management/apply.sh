10700e76cb3ca20b0c0b98f1a627425f863510d4
joydeep <defyjoy@users.noreply.github.com>
2025-03-31 01:50:44 +0530

    Fix: taskfile

--- Terraform/GCP/env/management/apply.sh
+++
@@ -1,5 +0,0 @@
-# gcloud config set project concise-mesh-453415-t8
-export TF_VAR_project_id=$(gcloud config get project)
-export TF_VAR_region="europe-west1"
-export TF_VAR_auth_token=$(gcloud auth print-access-token)
-terraform apply -auto-approve
